c213b6de897ee906954c01325d975a6aab9212a7
Sabula <76393947+Gemego@users.noreply.github.com>
2023-09-11 20:38:42 +0800

    Delete BraTS2020_training.sh

--- BraTS2020_training.sh
+++
@@ -1,3 +0,0 @@
-#! /mnt/diskB/zza/anaconda3/envs/MedSegDiff/bin/python3
-
-nohup python scripts/segmentation_train.py --data_dir data/BraTS2020_TrainingData --out_dir data/BraTS2020_output --image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16 --diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False --lr 1e-4 --batch_size 8 > BraTS2020.log 2>&1 &
